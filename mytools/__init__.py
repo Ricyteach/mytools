ea6afb90b046e701c37600d8332d1b8bf3a83115
Rick Teachey <ricky@teachey.org>
2019-06-12 18:22:44 -0400

    setup, flatten

--- mytools/__init__.py
+++ mytools/__init__.py
@@ -1,3 +1,6 @@
+from itertools import islice
+
+
 def flatten(iterable):
     """Flattens an iterable."""
     for i in iterable:
@@ -5,3 +8,9 @@ def flatten(iterable):
             yield from flatten(i)
         except TypeError:
             yield i
+
+
+def chunk(flat_list, sizes):
+    """Breaks a list into chunks - sort of the opposite of flatten."""
+    iter_flat = iter(flat_list)
+    yield from (list(islice(iter_flat, 0, size)) for size in sizes)
--- tests/test_mytools.py
+++ tests/test_mytools.py
@@ -1,6 +1,11 @@
 from typing import Iterable
 
-from mytools import flatten
+from mytools import flatten, chunk
+
 
 def test_flatten():
     assert not any(isinstance(x, Iterable) for x in flatten([1, 1, 2, [1, 2], [1, 2], 2, 3, [1, 2], 1, [1, 2], 4]))
+
+
+def test_chunk():
+    assert list(chunk([1,2,3,4,5], [2,3])) == [[1,2],[3,4,5]]
